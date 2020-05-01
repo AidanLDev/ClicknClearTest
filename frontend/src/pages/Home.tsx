6f77bc10fcc5f393c6fd7b3abbb03960c7d79750
Aidan <aidansoftwaredev@gmail.com>
2020-05-02 00:04:39 +0100

    Using express boilerplate

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from '../components/ui/Header';
 
 const Home = () => {
+  const [APIRes, setAPIRes] = useState('');
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
+  const callAPI = () => {
+    fetch('http://localhost:3001/testAPI')
+      .then((res) => res.text())
+      .then((res) => setAPIRes(res));
+  };
   return (
     <>
+      <p>{APIRes}</p>
       <Header />
     </>
   );
